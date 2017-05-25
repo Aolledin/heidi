1d0e5f97a7384bb8d4fe382206570aa3b364cd98
Mathias Bavarel <Mathias.Bavarel@etu.unige.ch>
2017-05-25 11:08:24 +0200

    ajout de la reconnaissance des ordres de Tita et d'un premier test

--- Sources/heidi.swift
+++ Sources/heidi.swift
@@ -2,27 +2,6 @@ import LogicKit
 
 //definition des coups de siflets
 
-//Syntaxe de Tita:
-/*
-
------------------
-court € Siflet
-
-----------------
-whee € Siflet
-
-
----------------------
-vide € SeqenceSiflements
-
-s € SequenceSiflement, c € Siflet
----------------------------------
-c::s € SequenceSiflements
-
-
-le '€' est le symbole appartient à. Je ne note pas ici tous les autres coups de sifflets possible, le principe est clair.
-vide est une liste vide, a::b est la concaténation d'un élément a avec une liste b*/
-
 
 let court = Value("Court")
 let whee  = Value("Whee")
@@ -31,6 +10,7 @@ let wheet = Value("Wheet")
 let wheeo = Value("Wheeo")
 let hee   = Value("Hee")
 let long  = Value("Long")
+let pause = Value("Pause")
 
 //definition des ordres de Heidi
 //Syntaxe de Heidi:
@@ -52,7 +32,7 @@ o € Ordre, s € SequenceOrdres
 -----------------------------
 o::s € SequenceOrdres
 
-*/
+le '€' est le symbole appartient à, vide est une liste vide, a::b est la concaténation d'un élément a avec une liste b*/
 
 let deponer   = Value("Deponer")
 let dretg     = Value("Dretg")
@@ -65,12 +45,155 @@ let sa_fermar = Value("Sa fermar")
 
 
 
+//reconnais qu'on a deponer au début de la liste.
+
+/*Sytaxe de Tita:
+
+------------------------
+court::court € OrdreTita
+
+-----------------------
+whee::who € OrdreTita
+
+etc.
 
 
+--------------------
+vide € SeqOrdreTita
+
+o € OrdreTita, s €  SeqOrdreTita
+--------------------------------
+o::pause::s € SeqOrdreTita
+
+ici, les :: sont la concaténation soit de deux listes entre elles, soit d'un élément et d'une liste.
 
 
+*/
+
+func reconnaisDeponer(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+
+      return
+        SeqSifl === List.cons(court, reste1) &&
+        reste1 === List.cons(court, reste2) &&
+        reste2 === List.cons(pause, reste)
+
+    }
+}
+
+func reconnaisDretg(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+
+      return
+        SeqSifl === List.cons(whee, reste1) &&
+        reste1 === List.cons(who, reste2) &&
+        reste2 === List.cons(pause, reste)
+
+    }
+}
+
+func reconnaisSanester(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+
+      return
+        SeqSifl === List.cons(wheet, reste1) &&
+        reste1 === List.cons(wheeo, reste2) &&
+        reste2 === List.cons(pause, reste)
+
+    }
+}
+
+func reconnaisDavent(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+      let reste3 = g["reste3"]
+      let reste4 = g["reste4"]
+
+      return
+        SeqSifl === List.cons(wheet, reste1) &&
+        reste1 === List.cons(wheeo, reste2) &&
+        reste2 === List.cons(wheet, reste3) &&
+        reste3 === List.cons(wheet, reste4) &&
+        reste4 === List.cons(pause, reste)&&
 
 
+    }
+}
+
+func reconnaisDavos(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+      let reste3 = g["reste3"]
+
+      return
+        SeqSifl === List.cons(who, reste1) &&
+        reste1 === List.cons(hee, reste2) &&
+        reste2 === List.cons(who, reste3) &&
+        reste3 === List.cons(pause, reste)&&
+    }
+}
+
+
+func reconnaisPlaun(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+      let reste3 = g["reste3"]
+      let reste4 = g["reste4"]
+
+      return
+        SeqSifl === List.cons(hee, reste1) &&
+        reste1 === List.cons(hee, reste2) &&
+        reste2 === List.cons(hee, reste3) &&
+        reste3 === List.cons(hee, reste4) &&
+        reste4 === List.cons(pause, reste)&&
+
+
+    }
+}
+
+func reconnaisReturnar(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+      let reste3 = g["reste3"]
+
+      return
+        SeqSifl === List.cons(whee, reste1) &&
+        reste1 === List.cons(whee, reste2) &&
+        reste2 === List.cons(wheet, reste3) &&
+        reste3 === List.cons(pause, reste)&&
+
+    }
+}
+
+
+func reconnaisSaFermar(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+
+      return
+        SeqSifl === List.cons(long, reste1) &&
+        reste4 === List.cons(pause, reste)&&
+
+    }
+}
 
 
 
--- Tests/heidiTests/heidiTests.swift
+++ Tests/heidiTests/heidiTests.swift
@@ -7,7 +7,22 @@ class heidiTests: XCTestCase {
     func testExample() {
     }
 
+    func testDeponer(){
+      let v = Variable(named: "v")
+      let goal = reconnaisDeponer(SeqSifl: v, reste: List.cons(long, List.empty) )
+      let expected = List.cons(court, List.cons(court, List.cons(pause, List.cons(long, List.empty))))
+      var trouve: Bool = false
+      for sub in solve(goal) {
+        trouve = true //vérifie qu'au moins une solution est trouvée (sans, si solve ne trouve rien, le test passe)
+        let r = sub.reified()
+        XCTAssert(r[v].equals(expected), "reconnaisDeponer is incorrect")
+      }
+      XCTAssert(trouve, "reconnaisDeponer found nothing")
+
+    }
+
     static var allTests = [
         ("testExample", testExample),
+        ("testDeponer", testDeponer)
     ]
 }
