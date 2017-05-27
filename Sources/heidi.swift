73a9b7fcad9eecac905224cbe882ebc4218f1024
Mathias Bavarel <Mathias.Bavarel@etu.unige.ch>
2017-05-27 11:24:55 +0200

    optimisation en gardant uniquement trois coups de siflets

--- Sources/heidi.swift
+++ Sources/heidi.swift
@@ -3,14 +3,14 @@ import LogicKit
 //definition des coups de siflets
 
 
-let court = Value("Court")
-let whee  = Value("Whee")
-let who   = Value("Who")
-let wheet = Value("Wheet")
-let wheeo = Value("Wheeo")
-let hee   = Value("Hee")
-let long  = Value("Long")
-let pause = Value("Pause")
+let court = Value("court")
+let whee  = Value("whee")
+let who   = Value("who")
+let wheet = Value("wheet")
+let wheeo = Value("wheeo")
+let hee   = Value("hee")
+let long  = Value("long")
+let pause = Value("pause")
 
 //definition des ordres de Heidi
 //Syntaxe de Heidi:
@@ -34,14 +34,14 @@ o::s € SequenceOrdres
 
 le '€' est le symbole appartient à, vide est une liste vide, a::b est la concaténation d'un élément a avec une liste b*/
 
-let deponer   = Value("Deponer")
-let dretg     = Value("Dretg")
-let sanester  = Value("Sanester")
-let davent    = Value("Davent")
-let davos     = Value("Davos")
-let plaun     = Value("Plaun")
-let returnar  = Value("Returnar")
-let sa_fermar = Value("Sa fermar")
+let deponer   = Value("deponer")
+let dretg     = Value("dretg")
+let sanester  = Value("sanester")
+let davent    = Value("davent")
+let davos     = Value("davos")
+let plaun     = Value("plaun")
+let returnar  = Value("returnar")
+let sa_fermar = Value("sa_fermar")
 
 
 
@@ -273,6 +273,191 @@ func traducteur(OrdreHeidi: Term, OrdreTita: Term) -> Goal {
         traducteur(OrdreHeidi: resteHeidi, OrdreTita: resteTita)
     }
 }
+
+
+//Optimisation
+
+/*
+Syntaxe/Sémantique
+Je ne vais pas réécrire la syntaxe/sémantique car elle est exactement la même que celle faite plus haut.
+*/
+
+
+//Niveau implémentation, pour cette partie, on ne reconnaitra pas les pauses dans les fonctions reconnaissant les séquences de siflements,
+//mais plutôt dans une fonction à part, pour faciliter l'implémentation de l'enlevage de pause.
+//On garde par contre toujours une pause à la fin.
+
+func reconnaisPause(SeqSifl: Term, reste: Term) -> Goal{
+    return
+      SeqSifl === List.cons(pause, reste)
+
+}
+
+
+func reconnaisDeponer2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+
+
+      return
+        SeqSifl === List.cons(wheeo, reste1) &&
+        reste1 === List.cons(hee, reste2) &&
+        reste2 === List.cons(wheet, reste)
+    }
+}
+
+
+func reconnaisDretg2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+
+
+      return
+        SeqSifl === List.cons(hee, reste1) &&
+        reste1 === List.cons(wheet, reste)
+
+    }
+}
+
+
+func reconnaisSanester2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+
+
+      return
+        SeqSifl === List.cons(wheet, reste1) &&
+        reste1 === List.cons(wheeo, reste)
+    }
+}
+
+
+func reconnaisDavent2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+
+
+      return
+        SeqSifl === List.cons(wheet, reste1) &&
+        reste1 === List.cons(hee, reste2) &&
+        reste2 === List.cons(wheet, reste)
+    }
+}
+
+
+
+func reconnaisDavos2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+
+      return
+        SeqSifl === List.cons(wheet, reste1) &&
+        reste1 === List.cons(wheeo, reste2) &&
+        reste2 === List.cons(wheet, reste)
+
+    }
+}
+
+
+func reconnaisPlaun2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+      let reste2 = g["reste2"]
+
+      return
+        SeqSifl === List.cons(wheet, reste1) &&
+        reste1 === List.cons(wheeo, reste2) &&
+        reste2 === List.cons(wheeo, reste)
+
+
+    }
+}
+
+
+func reconnaisReturnar2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+
+      return
+        SeqSifl === List.cons(wheeo, reste1) &&
+        reste1 === List.cons(wheet, reste)
+    }
+}
+
+
+func reconnaisSaFermar2(SeqSifl: Term, reste: Term) -> Goal{
+  return
+    freshn {g in
+      let reste1 = g["reste1"]
+
+      return
+        SeqSifl === List.cons(wheeo, reste1) &&
+        reste1 === List.cons(wheeo, reste)
+    }
+}
+
+
+
+func traducteur2(OrdreHeidi: Term, OrdreTita: Term) -> Goal {
+  return (OrdreHeidi === List.empty && OrdreTita === List.empty) || //cas terminal
+    freshn{g in
+      let resteHeidi = g["resteHeidi"]
+      let resteTitaSansPause = g["resteTitaSansPause"]
+      let resteTitaAvecPause = g["resteTitaAvecPause"]
+      let premierOrdreHeidi = g["premierOrdreHeidi"]
+
+      return //idem qu'avant, on ajoute juste la pause à la fin.
+        OrdreHeidi === List.cons(premierOrdreHeidi, resteHeidi) && (
+          (premierOrdreHeidi === deponer && reconnaisDeponer2(SeqSifl: OrdreTita, reste: resteTitaAvecPause)) ||
+          (premierOrdreHeidi === dretg && reconnaisDretg2(SeqSifl: OrdreTita, reste: resteTitaAvecPause)) ||
+          (premierOrdreHeidi === sanester && reconnaisSanester2(SeqSifl: OrdreTita, reste: resteTitaAvecPause)) ||
+          (premierOrdreHeidi === davent && reconnaisDavent2(SeqSifl: OrdreTita, reste: resteTitaAvecPause)) ||
+          (premierOrdreHeidi === davos && reconnaisDavos2(SeqSifl: OrdreTita, reste: resteTitaAvecPause)) ||
+          (premierOrdreHeidi === plaun && reconnaisPlaun2(SeqSifl: OrdreTita, reste: resteTitaAvecPause)) ||
+          (premierOrdreHeidi === returnar && reconnaisReturnar2(SeqSifl: OrdreTita, reste: resteTitaAvecPause)) ||
+          (premierOrdreHeidi === sa_fermar && reconnaisSaFermar2(SeqSifl: OrdreTita, reste: resteTitaAvecPause))
+        ) &&
+        reconnaisPause(SeqSifl: resteTitaAvecPause, reste: resteTitaSansPause) &&
+        traducteur(OrdreHeidi: resteHeidi, OrdreTita: resteTitaSansPause)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Tests/heidiTests/heidiTests.swift
+++ Tests/heidiTests/heidiTests.swift
@@ -9,6 +9,7 @@ class heidiTests: XCTestCase {
 
 
     //Je ne fais pas de tests pour les autres ordres, étant donné que les fonctions sont des copier/coller de la première.
+    //La structure du test est classique: une variable libre, une fonction.
     func testDeponer(){
       let v = Variable(named: "v")
       let goal = reconnaisDeponer(SeqSifl: v, reste: List.cons(long, List.empty) )
@@ -32,7 +33,76 @@ class heidiTests: XCTestCase {
       var trouve : Bool = false
       for sub in solve(goal) {
         let r = sub.reified()
-        print(r[v]) 
+        //print(r[v]) //À décomenter pour voir la traduction
+        trouve = true
+        XCTAssert(r[v].equals(expected), "traducteur is incorrect")
+      }
+      XCTAssert(trouve, "traducteur found nothing")
+    }
+
+
+    //Double traduction, on solve deux fois deux expressions différentes pour traduire dans les deux sens, on espère retrouver la même solution.
+    func testTraducteurDouble() {
+      let v = Variable(named: "v")
+
+      let aTraduire = List.cons(dretg, List.cons(dretg, List.cons(sa_fermar, List.cons(returnar, List.empty))))
+      let goal = traducteur(OrdreHeidi: aTraduire, OrdreTita: v)
+      let expected = aTraduire
+      var trouve : Bool = false
+      for sub in solve(goal) {
+        let r = sub.reified()
+        let w = Variable(named: "w")
+        let aTraduire2 = r[v]
+        let goal2 = traducteur(OrdreHeidi: w, OrdreTita: aTraduire2)
+        for sub in solve(goal2){
+          let s = sub.reified()
+          trouve = true
+          XCTAssert(s[w].equals(expected), "traducteur is incorrect")
+        }
+
+      }
+      XCTAssert(trouve, "traducteur found nothing")
+
+    }
+
+
+    //Plus ou moins les même tests qu'à la première partie
+    func testDretg2(){
+      let v = Variable(named: "v")
+      let goal = reconnaisDretg2(SeqSifl: v, reste: List.cons(long, List.empty) )
+      let expected = List.cons(hee, List.cons(wheet, List.cons(long, List.empty)))
+      var trouve: Bool = false
+      for sub in solve(goal) {
+        trouve = true
+        let r = sub.reified()
+        XCTAssert(r[v].equals(expected), "reconnaisDeponer is incorrect")
+      }
+      XCTAssert(trouve, "reconnaisDeponer found nothing")
+    }
+
+    func testPause(){
+      let v = Variable(named: "v")
+      let goal = reconnaisPause(SeqSifl: List.cons(pause, List.cons(pause, List.cons(whee, List.empty ))), reste: v)
+      let expected = List.cons(pause, List.cons(whee,  List.empty))
+      var trouve: Bool = false
+      for sub in solve(goal) {
+        trouve = true
+        let r = sub.reified()
+        XCTAssert(r[v].equals(expected), "reconnaisDeponer is incorrect")
+      }
+      XCTAssert(trouve, "reconnaisDeponer found nothing")
+    }
+
+
+    func testTraducteur2(){
+      let v = Variable(named: "v")
+      let aTraduire = List.cons(deponer, List.cons(sanester,  List.cons(davos, List.empty)))
+      let goal = traducteur2(OrdreHeidi: aTraduire, OrdreTita: v)
+      let expected = List.cons(wheeo, LogicKit.List.cons(hee, LogicKit.List.cons(wheet, LogicKit.List.cons(pause, LogicKit.List.cons(wheet, LogicKit.List.cons(wheeo, LogicKit.List.cons(pause, LogicKit.List.cons(who, LogicKit.List.cons(hee, LogicKit.List.cons(who, LogicKit.List.cons(pause, LogicKit.List.empty)))))))))))
+      var trouve : Bool = false
+      for sub in solve(goal) {
+        let r = sub.reified()
+        //print(r[v]) //À décomenter pour voir la traduction
         trouve = true
         XCTAssert(r[v].equals(expected), "traducteur is incorrect")
       }
@@ -52,14 +122,14 @@ class heidiTests: XCTestCase {
 
 
 
-
-
-
-
-
     static var allTests = [
         ("testExample", testExample),
         ("testDeponer", testDeponer),
         ("testTraducteur", testTraducteur),
+        ("testTraducteurDouble", testTraducteurDouble),
+        ("testDretg2", testDretg2),
+        ("testPause", testPause),
+        ("testTraducteur2", testTraducteur2),
+
     ]
 }
